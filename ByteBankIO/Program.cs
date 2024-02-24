f68ce4abbc07271b3b05dfd08c86191848afbdcb
Matheus Canovas <matheuscanovas9@gmail.com>
2024-02-24 12:50:24 -0300

    feat: Reading text files w/ FileStream & encoding

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,4 +1,27 @@
-﻿using ByteBankIO;
+﻿using System.Text;
 
+class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = "accounts.txt";
+        int totalReadedBytes = -1;
+        FileStream stream = new(filePath, FileMode.Open);
 
-Console.ReadLine();
+        byte[] buffer = new byte[1024]; // 1kb
+
+        while (totalReadedBytes != 0)
+        {
+            totalReadedBytes = stream.Read(buffer, 0, 1024);
+            WriteBuffer(buffer);
+        }
+    }
+
+    static void WriteBuffer(byte[] buffer)
+    {
+        UTF8Encoding utf8 = new();
+        string text = utf8.GetString(buffer);
+
+        Console.Write(text);
+    }
+}
